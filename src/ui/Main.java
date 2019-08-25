00c6a6e40c7c7b5e6dd83dfb1b973bc237f28d19
Christian Flor <christian.flor1@correo.icesi.edu.co>
2019-08-25 17:32:44 -0500

    Interface implemented

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,5 +1,31 @@
 package ui;
 
-public class Main {
+import java.io.IOException;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public static void main(String[] args) {
+		
+		launch();
+	}
+
+	@Override
+	public void start(Stage stage) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("gui.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		stage.setTitle("SPACE WAR");
+		stage.setScene(scene);
+		stage.setMaximized(true);
+		stage.show();
+	}
+
 
 }
--- src/ui/SpaceWarController.java
+++ src/ui/SpaceWarController.java
@@ -1,5 +1,76 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+
 public class SpaceWarController {
+
+    @FXML
+    private GridPane gridP1FW;
+
+    @FXML
+    private GridPane gridP2FW;
+
+    @FXML
+    private GridPane gridP3FW;
+
+    @FXML
+    private TextArea rowFW;
+
+    @FXML
+    private TextArea columnFW;
+
+    @FXML
+    private GridPane gridP1SW;
+
+    @FXML
+    private GridPane gridP2SW;
+
+    @FXML
+    private GridPane gridP3SW;
+
+    @FXML
+    private TextArea rowSW;
+
+    @FXML
+    private TextArea columnSW;
+
+    @FXML
+    private GridPane gridP1TW;
+
+    @FXML
+    private GridPane gridP2TW;
+
+    @FXML
+    private GridPane gridP3TW;
+
+    @FXML
+    private TextArea amountMatrixTW;
+
+    @FXML
+    private Button generateTW;
+
+    @FXML
+    void generateFW(ActionEvent event) {
+
+    }
+
+    @FXML
+    void generateSW(ActionEvent event) {
+
+    }
+
+    @FXML
+    void noRepeSW(ActionEvent event) {
+
+    }
+
+    @FXML
+    void withRepeSW(ActionEvent event) {
+
+    }
 
 }
